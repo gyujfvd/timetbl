a105b83ce79d4044c9e95ef02b4f8f33bb9d7b3f
gyujfvd <106913513+gyujfvd@users.noreply.github.com>
2023-08-27 11:36:38 +0300

    Update script.js

--- style/script.js
+++ style/script.js
@@ -3,4 +3,9 @@ const tg = window.Telegram.WebApp;
 const userName = tg.initDataUnsafe.user.first_name;
 
 let p = document.getElementById('0');
-p.textContent = `Привет, ${userName}`;
+let but = document.getElementById('save');
+but.addEventListener('click', () => {
+    console.log(document.getElementsByClassName('input'));
+
+});
+
