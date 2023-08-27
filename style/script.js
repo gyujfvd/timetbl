0bee3bb428b9bac75aea51a2d22d1669cff04c90
gyujfvd <106913513+gyujfvd@users.noreply.github.com>
2023-08-27 10:44:32 +0300

    Update script.js

--- style/script.js
+++ style/script.js
@@ -1,6 +1,6 @@
 const tg = window.Telegram.WebApp;
+
 const userName = tg.initDataUnsafe.userName.first_name;
 
-let p = document.getElementById('0')
-
-p.textContent = userName
+let p = document.getElementById('0');
+p.textContent = `Привет, ${userName}`;
